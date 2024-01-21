ce2237d81f2b39d8d410ea87bc381c3224d961ff
Jérôme Peyras <jerome.peyras@protonmail.com>
2024-01-21 12:14:36 +0100

    feat: Add logo to main menu

--- src/views/main_menu_screen.py
+++ src/views/main_menu_screen.py
@@ -10,7 +10,13 @@ def main_menu_screen() -> str:
         "3": "Afficher tous les joueurs",
         "q": "Quitter",
     }
-    print("Bienvenue dans ChessCenter :")
+    print("""
+  ____ _                      ____           _            
+ / ___| |__   ___  ___ ___   / ___|___ _ __ | |_ ___ _ __ 
+| |   | '_ \ / _ \/ __/ __| | |   / _ \ '_ \| __/ _ \ '__|
+| |___| | | |  __/\__ \__ \ | |__|  __/ | | | ||  __/ |   
+ \____|_| |_|\___||___/___/  \____\___|_| |_|\__\___|_|   """)
+    print()
     [print(f"{option}: {options[option]}") for option in options.keys()]
 
     return input("Que voulez-vous faire? ")
