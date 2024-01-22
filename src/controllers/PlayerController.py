0b40d1455432deb3f5981f3c015576e5d54ba154
Jérôme Peyras <jerome.peyras@protonmail.com>
2024-01-22 16:49:03 +0100

    fix: Handle no file or no players for leaderboard

--- src/controllers/PlayerController.py
+++ src/controllers/PlayerController.py
@@ -2,6 +2,8 @@ from models.PlayerModel import PlayerModel
 from models.TournamentModel import TournamentModel
 from views.player.PlayerViews import PlayerViews
 from views.tournament.TournamentViews import TournamentViews
+from views.shared.alert_message import alert_message
+from utils.errors import LoadError
 
 
 class PlayerController:
@@ -17,13 +19,21 @@ class PlayerController:
     def show_tournament_players(self, tournament_id: str) -> None:
         """Show players from a specific tournament"""
         tournament = TournamentModel.load_by_id(tournament_id)
-        tournament_players = PlayerModel.get_tournament_players(
-            tournament_id=tournament_id
-        )
-        TournamentViews().show(tournament=tournament)
-        self.views.leaderboard(players=tournament_players)
+        try:
+            tournament_players = PlayerModel.get_tournament_players(
+                tournament_id=tournament_id
+            )
+            if len(tournament_players) <= 0:
+                alert_message(
+                    message="Aucun joueur n'est inscrit au tournoi", type="Info")
+            else:
+                TournamentViews().show(tournament=tournament)
+                self.views.leaderboard(players=tournament_players)
 
-        input("Appuyez sur [Entrée] pour continuer.")
+                input("Appuyez sur [Entrée] pour continuer.")
+        except LoadError:
+            alert_message(
+                message="Aucun fichier de joueurs n'a été trouvé, si vous venez de créer le tournoi essayez d'ajouter des joueurs.")
 
     def add_player_to_tournament(self, tournament_id: str) -> None:
         """Adds a player to a given tournament"""
